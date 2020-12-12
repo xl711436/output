075f3c52d2ccf8ad5c0b94b5c2d2860d2721ea8f
xl <xl711436@hotmail.com>
2020-12-12 10:21:23 +0800

    添加 vscode  java项目示例

---
+++ vscode_java/vscodetest/src/main/java/com/xl/App.java
@@ -0,0 +1,19 @@
+package com.xl;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String aa ="33";
+        System.out.println(aa +aa);
+    }
+}
