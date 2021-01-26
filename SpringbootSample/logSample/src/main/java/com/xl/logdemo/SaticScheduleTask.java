ef6a28a1038ab2dbef5082fd6512c63b2fc2e6ba
xl <xl711436@hotmail.com>
2021-01-27 00:24:00 +0800

    添加日志和 定时任务的文档

---
+++ SpringbootSample/logSample/src/main/java/com/xl/logdemo/SaticScheduleTask.java
@@ -0,0 +1,25 @@
+package com.xl.logdemo;
+
+import com.xl.logdemo.controller.HelloWorldController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.LocalDateTime;
+
+@Configuration      //1.主要用于标记配置类，兼备Component的效果。
+@EnableScheduling   // 2.开启定时任务
+public class SaticScheduleTask {
+
+    private static Logger logger = LogManager.getLogger(SaticScheduleTask.class.getName());
+
+    //3.添加定时任务
+    @Scheduled(cron = "0/5 * * * * ?")
+    //或直接指定时间间隔，例如：5秒
+    //@Scheduled(fixedRate=5000)
+    private void configureTasks() {
+        logger.trace("执行静态定时任务时间: " + LocalDateTime.now());
+    }
+}
--- SpringbootSample/logSample/src/main/java/com/xl/logdemo/controller/HelloWorldController.java
+++ SpringbootSample/logSample/src/main/java/com/xl/logdemo/controller/HelloWorldController.java
@@ -1,10 +1,34 @@
 package com.xl.logdemo.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 @RestController
 @RequestMapping("/hello")
 public class HelloWorldController {
+
+
+    @Value("${server.port}")
+    public String port;
+
+    @Value("${dd.bb}")
+    public String bb;
+
+    private static Logger logger = LogManager.getLogger(HelloWorldController.class.getName());
+
+
     @RequestMapping("/say")
     public String say(){
-        return "Hello World";
+        logger.trace("trace");
+        logger.info("info");
+        logger.error("error");
+
+        return "Hello World" + port+ " " + bb;
     }
 }
